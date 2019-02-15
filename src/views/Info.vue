30b3d4ff001dad176548ddd70d6ae86a2d2c397e
田乐 <tianle@klldeMacBook-Pro.local>
2019-02-15 15:07:38 +0800

    first commit

---
+++ src/views/Info.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      info
+      <button @click="add()">弹出</button>
+    </div>
+</template>
+
+<script>
+  import store from '@/store'
+  export default {
+    name: 'Info',
+    store,
+    methods: {
+      add(){
+        console.log('add item')
+        store.commit('increase')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
